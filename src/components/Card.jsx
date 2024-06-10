48f1a1965b22136463ec3c020441eeeb96dc8f47
solaim007 <solaim2629@gmail.com>
2024-06-11 00:16:20 +0530

    second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ()=>{
     const Weather = useWeather();
     return (
         <div className="card">
-            <img src={Weather?.data?.current?.condition?.icon}/>
+            <img src={Weather?.data?.current?.condition?.icon} alt="icon"/>
             <h2>{Weather.data?.current?.temp_c}C</h2>
             <h5>{Weather?.data?.location?.name}, {Weather?.data?.location?.region}, {Weather?.data?.location?.country}</h5>
 
